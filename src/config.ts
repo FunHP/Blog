8932d876eee6785b30a0bcdde4ce1679795711be
FunHP <FunHP_PH@outlook.com>
2026-04-20 13:34:55 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ export const siteConfig: SiteConfig = {
 		src: "https://from1996.fun/PicGo/中世纪-孤独的女孩.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
+			enable: true, // Display the credit text of the banner image
 			text: "111", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
